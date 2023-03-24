4d1ce69a332b2db76e92a84da8728828b4de2f71
Elias Ladenburger <elias@ladenburger-family.de>
2023-03-24 19:19:16 +0100

    Set up skeleton

---
+++ src/lib/Header.svelte
@@ -0,0 +1,18 @@
+<script>
+    let home = {name: "Home", slug: ""}
+    let about = {name: "About", slug: "about"}
+    let community = {name: "Community", slug: "community"}
+    let contact = {name: "Contact", slug: "contact"}
+
+    export let menu = [home, about, community, contact]
+</script>
+
+<nav>
+    <ul class="list-reset md:flex justify-end flex-1 items-center bg-dark text-orangeDark">
+        {#each menu as navElem}
+        <li class="mr-5">
+            <a class="inline-block py-2 px-4 font-bold no-underline hover:opacity-70" href="/{navElem.slug}" on:click={()=>showOnMobile="hidden"}>{navElem.name}</a>
+        </li>
+        {/each}
+    </ul>
+</nav>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{svelte,js,html}"],
   theme: {
     extend: {},
+    colors: {
+      dark: "rgb(22, 22, 22)",
+      light: "rgb(247, 247, 247)",
+      orangeDark: "rgb(176, 80, 32)",
+    }
   },
   plugins: [],
 }
