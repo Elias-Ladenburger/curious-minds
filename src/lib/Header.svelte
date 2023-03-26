791e5db4d5ed5323449fb077eb7656fea7846e8e
Elias Ladenburger <elias@ladenburger-family.de>
2023-03-26 18:54:19 +0200

    Add new favicon

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -12,7 +12,7 @@
     <ul class="list-reset md:flex justify-end flex-1 items-center bg-dark text-orangeDark font-header">
         {#each menu as navElem}
         <li class="mr-5">
-            <a class="inline-block py-2 px-4 font-bold no-underline hover:opacity-70" href="/{navElem.slug}" on:click={()=>showOnMobile="hidden"}>{navElem.name}</a>
+            <a class="inline-block py-2 px-4 font-bold no-underline hover:opacity-70" href="/{navElem.slug}">{navElem.name}</a>
         </li>
         {/each}
     </ul>
