72f1893fa6a9780fa653042e39d0531c903c81d4
Elias Ladenburger <elias@ladenburger-family.de>
2023-03-26 22:04:10 +0200

    Make menu header menu responsive

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -4,15 +4,34 @@
         {name: "About", slug: "about"},
         //let community = {name: "Community", slug: "community"}
         {name: "Contact", slug: "contact"}
-    ]
+    ];
+
+    $: showOnMobile = "hidden";
+    $: buttonSymbol = "&#9776;"
+
+    function showMenuOnMobile() {
+        let showOnMobileClasses = ["hidden", "w-full h-screen"];
+        showOnMobile = showOnMobile === showOnMobileClasses[0] ? showOnMobileClasses[1] : showOnMobileClasses[0];
+        buttonSymbol = buttonSymbol === "&times;" ? "&#9776;" : "&times;";
+    }
 
 </script>
 
-<nav>
-    <ul class="list-reset md:flex justify-end flex-1 items-center bg-dark text-orangeDark font-header">
+<nav class="">
+    <div class="">
+        <!-- This is used to open the menu on mobile devices -->
+<div class="md:hidden">
+    <button class="text-primaryContrast text-4xl font-bold duration-300 hover:opacity-70 mx-2"
+        on:click="{()=>showMenuOnMobile()}">
+            {@html buttonSymbol}
+    </button>
+</div>
+    </div>
+    <ul class=" {showOnMobile} list-reset md:flex justify-end flex-1 items-center bg-dark text-orangeDark font-header">
+        
         {#each menu as navElem}
         <li class="mr-5">
-            <a class="inline-block py-2 px-4 font-bold no-underline hover:opacity-70" href="/{navElem.slug}">{navElem.name}</a>
+            <a class="inline-block py-2 px-4 font-bold no-underline hover:opacity-70" href="/{navElem.slug}" on:click="{showMenuOnMobile}">{navElem.name}</a>
         </li>
         {/each}
     </ul>
